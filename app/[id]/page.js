811d8bc2ea8b54e467eae3a1ff660f2a77a8e856
mryunt02 <mrthresh@gmail.com>
2024-04-30 19:57:30 +0300

    page

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,24 +1,39 @@
 import { Suspense } from "react";
 import Details from "@/components/Details";
 
-async function getData({ params }) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  debugger;
+export async function getStaticPaths() {
+  // Replace this with your actual data fetching logic
+  const agents = await fetch("https://valorant-api.com/v1/agents");
+  const data = await agents.json();
+
+  const paths = data.data.map((agent) => ({
+    params: { id: agent.uuid },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
   const response = await fetch(
     `https://valorant-api.com/v1/agents/${params.id}`
   );
   const repo = await response.json();
-  return repo.data;
-}
-async function DetailPage({ params }) {
-  const agent = await getData({ params });
 
+  return {
+    props: {
+      agent: repo.data,
+    },
+  };
+}
+
+function DetailPage({ agent }) {
   return <Details agent={agent} />;
 }
-export default function AgentDetail({ params }) {
+
+export default function AgentDetail({ agent }) {
   return (
     <main>
-      <DetailPage params={params} />
+      <DetailPage agent={agent} />
     </main>
   );
 }
